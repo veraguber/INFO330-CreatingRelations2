3d54abbbbcf79f22ff0aee049f087d7bb3ce56ea
VERAGUBER <VERA2@UW.EDU>
2023-04-26 14:30:24 -0700

    Extra credit insert statements

---
+++ EC.sql
@@ -0,0 +1,6 @@
+INSERT INTO imported_pokemon_data (name, pokedex_number, type1) VALUES ('Huskichu', 802, 'Mascot');
+INSERT INTO imported_pokemon_data (name, pokedex_number, type1) VALUES ('Cougarite', 803, 'Mascot');
+
+INSERT INTO Base_Stats(pokedex_number, attack, defense, sp_attack, sp_defense, speed, hp) VALUES (802, 125, 80, 150, 110, 120, 103);
+INSERT INTO Base_Stats(pokedex_number, attack, defense, sp_attack, sp_defense, speed, hp) VALUES (803, 1, 3, 2, 1, 5, 3);
+
