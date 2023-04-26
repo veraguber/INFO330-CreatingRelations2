b6fd057556e51da5f142b06a96b5c76353713b52
VERAGUBER <VERA2@UW.EDU>
2023-04-26 14:36:36 -0700

    More ec insert statements

--- EC.sql
+++ EC.sql
@@ -4,3 +4,6 @@ INSERT INTO imported_pokemon_data (name, pokedex_number, type1) VALUES ('Cougari
 INSERT INTO Base_Stats(pokedex_number, attack, defense, sp_attack, sp_defense, speed, hp) VALUES (802, 125, 80, 150, 110, 120, 103);
 INSERT INTO Base_Stats(pokedex_number, attack, defense, sp_attack, sp_defense, speed, hp) VALUES (803, 1, 3, 2, 1, 5, 3);
 
+INSERT INTO Abilities (id, ability_name) VALUES (802, 'Cherry Blossom');
+INSERT INTO Abilities (id, ability_name) VALUES (802, 'Purple Gold');
+INSERT INTO Abilities (id, ability_name) VALUES (803, 'Slow Attack');
